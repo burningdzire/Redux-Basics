7fa29ba566af87697f7c33a2347ce2039acfecd0
SharmaMohit <mohit.sharma@tatahealth.com>
2018-11-26 11:51:36 +0530

    Adding json object as state

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 import { createStore } from 'redux';
 
-const reducer = (state, action) => {
+const initialState = {
+    result: 1,
+    lastValues: []
+};
+
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD":
-        state = state + action.payload;
+            state = {
+                ...state,
+                result: state.result + action.payload
+            }
             break;
         case "SUBTRACT":
-        state = state - action.payload;
+            state = {
+                ...state,
+                result: state.result - action.payload
+            }
+            break;
+        case "MULTIPLY":
+            state = {
+                ...state,
+                result: state.result * action.payload
+            }
+            break;
+        case "DIVIDE":
+            state = {
+                ...state,
+                result: state.result / action.payload
+            }
+            break;
+        case "REMAINDER":
+            state = {
+                ...state,
+                result: state.result % action.payload
+            }
             break;
         default:
             break;
@@ -21,7 +50,7 @@ const reducer = (state, action) => {
     return state;
 }
 
-const store = createStore(reducer, 1);
+const store = createStore(reducer);
 
 store.subscribe(() => {
     console.log("Store Updated!!", store.getState());
