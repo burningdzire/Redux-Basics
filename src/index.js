98f20bf802a316bc808d2958a371ae55b98d3ea9
SharmaMohit <mohit.sharma@tatahealth.com>
2018-11-26 11:31:20 +0530

    Understanding Concept of Store in REdux

--- src/index.js
+++ src/index.js
@@ -4,5 +4,38 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "ADD":
+        state = state + action.payload;
+            break;
+        case "SUBTRACT":
+        state = state - action.payload;
+            break;
+        default:
+            break;
+    }
+    return state;
+}
+
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+    console.log("Store Updated!!", store.getState());
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 10
+});
+
+
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 7
+});
+
 serviceWorker.unregister();
